a76a63e23083f9279e95c04f03c24aadf609c820
khateejaPasha <khateejapasha11@gmail.com>
2024-02-12 17:33:40 +0530

    Added variable in class

--- TestProject/src/TestPackage/Test.java
+++ TestProject/src/TestPackage/Test.java
@@ -1,10 +1,13 @@
 package TestPackage;
 
 public class Test {
-
+int a=8;
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		//Test t=new Test(a);
+		int a=8;
 System.out.println("hello world");
+System.out.println(a);
 	}
 
 }
